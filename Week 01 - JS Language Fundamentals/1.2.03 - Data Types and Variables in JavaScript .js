e262f09b969dafd7ff04ddb906d31724a7b45a0e
Niels Lange <info@nielslange.de>
2019-01-21 07:26:14 +0700

    Add 1.2.03 - Data Types and Variables in JavaScript .js

---
+++ Week 01 - JS Language Fundamentals/1.2.03 - Data Types and Variables in JavaScript .js
@@ -0,0 +1,15 @@
+// 1.2.3.1 - Declaring and assigning a variable
+
+var username;
+username = 'nielslange';
+console.log( nielslange );
+
+// 1.2.3.1 - Declaring and assigning a variable together
+
+var username = 'nielslange';
+console.log( nielslange );
+
+// 1.2.3.3 - An undefined variable
+
+var username;
+console.log( nielslange );
