38d967a6e52e85e6686234eb34d18fff337fb87f
Niels Lange <info@nielslange.de>
2019-01-21 07:54:53 +0700

    Add 1.2.10 – Strong versus Weak Typing and typeof .js

---
+++ Week 01 - JS Language Fundamentals/1.2.10 – Strong versus Weak Typing and typeof .js
@@ -0,0 +1,21 @@
+// 1.2.10.1 - Weak / loose typing in  JavaScript
+
+var container = 'Text';
+console.log( typeof container );
+
+container = 2;
+console.log( typeof container );
+
+// 1.2.10.2 - More weak typing in JavaScript
+
+var container = 'Text';
+console.log( typeof container );
+
+container = 2;
+console.log( typeof container );
+
+container = true;
+console.log( typeof container );
+
+container = { id: 1, title: 'Hello' };
+console.log( typeof container );
