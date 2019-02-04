e4defe2549b1d30e1fd991d5e46fc3f1c324fd21
Niels Lange <info@nielslange.de>
2019-02-04 17:23:18 +0700

    Added examples 1.3.10 to week 02

---
+++ Week 02 - The DOM & Handling Events/1.3.10/app.js
@@ -0,0 +1,192 @@
+/********************************
+ * Adding Nodes to the DOM
+ * 1.3.10
+ *
+ *******************************/
+
+
+
+
+
+
+
+/********************************
+ * Adding Nodes to the DOM
+ * with appendChild
+ * 1.3.10.1
+ *
+ *******************************/
+
+
+// var content = document.querySelector('.content'),
+//     pText = document.createTextNode( 'Mas Lorem ' ),
+//     pEl = document.createElement('p');
+//
+//
+// pEl.appendChild( pText );
+// content.appendChild( pEl );
+//
+// console.log( content.outerHTML );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Adding Nodes to the DOM
+ * with insertBefore
+ * 1.3.10.2
+ *
+ *******************************/
+
+
+// var content = document.querySelector('.content'),
+//     firstP = content.children[ 1 ],
+//     pText = document.createTextNode( 'Newum' ),
+//     pEl = document.createElement('p');
+//
+//
+// pEl.appendChild( pText );
+// content.insertBefore( pEl, firstP );
+//
+// console.log( content.outerHTML );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * How to Inser After an Element
+ * Using .nextSibling
+ * 1.3.10.3
+ *
+ *******************************/
+
+
+// var content = document.querySelector('.content'),
+//    firstP = content.children[ 1 ],
+//    pText = document.createTextNode( 'Newum' ),
+//    pEl = document.createElement('p');
+//
+//
+// pEl.appendChild( pText );
+// content.insertBefore( pEl, firstP.nextSibling );
+//
+// console.log( content.outerHTML );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Replacing Nodes with
+ * replaceChild
+ * 1.3.10.3
+ *
+ *******************************/
+
+//
+// var content = document.querySelector('.content'),
+//     firstP = content.children[ 1 ],
+//     pText = document.createTextNode( 'Newum' ),
+//     pEl = document.createElement('p');
+//
+//
+// pEl.appendChild( pText );
+// content.replaceChild( pEl, firstP );
+//
+// console.log( content.outerHTML );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// End 1.3.10
