adce1ee756ab81186bcaee550b4d32111e730d6e
Niels Lange <info@nielslange.de>
2019-02-04 17:17:13 +0700

    Update tasks.js for week 03

--- Week 03 - API Calls, JSON & Local Storage/tasks.js
+++ Week 03 - API Calls, JSON & Local Storage/tasks.js
@@ -1,1 +1,40 @@
-// Week 03 - API Calls, JSON & Local Storage #################################################################################
+// Week 03 - API Calls, JSON & Local Storage ########################################
+
+// 1. Create a JSON string of 3 post objects.
+
+// 2. Create a JSON string of 3 post objects then render them to the page with the following markup
+// <article class="post">
+//  <h2 class="entry-title"><a href="#">Title</a></h2>
+//  <div class="entry-excerpt">
+//    <p>Excerpt here...</p>
+//  </div>
+//</article>
+
+// 3. Create a JSON string of 3 post objects and save them in local storage
+
+// 4. Create a JSON string of 3 post objects and save them in local storage then get them from local storage and render them to the page
+
+// 5. Create an API call to a working WordPress site and render 3 posts to the page
+
+// 6. Create an API call to a working WordPress site and get 3 images and render them to the page
+
+// 7. Create an interface that let's you enter a URL for a WordPress site in a form and when you click submit it displays the latest 10 posts from that site
+
+// 8. Extend the previous example to make a blog reader that let's you click on a post and read the content of it. Also display the featured image if one exists.
+
+// 9. Build a simple de-coupled WordPress site that lists pages and posts and let's you view them without leaving or refreshing the page.
+
+// 10. Look over this list of [Open APIs](https://github.com/toddmotto/public-apis) and choose one to make an API call to and display information from
+
+// Optional #########################################################################
+
+// Excercise 1.5.2
+// Create a JSON object for a page
+// Parse teh JSON object into a native JS object
+// Add the title and content to an HTML page
+
+// Excercise 1.5.3
+// Create a JSON object with post data
+// Save to local storage
+// Get post data from local storage
+// Display title and content of post on the page
