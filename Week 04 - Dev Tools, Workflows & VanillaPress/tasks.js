1feb980348a68450e38fac7fa9bd561bfe4b0c16
Niels Lange <info@nielslange.de>
2019-02-04 17:19:12 +0700

    Update tasks.js for week 04

--- Week 04 - Dev Tools, Workflows & VanillaPress/tasks.js
+++ Week 04 - Dev Tools, Workflows & VanillaPress/tasks.js
@@ -1,1 +1,25 @@
-// Week 04 - Dev Tools, Workflows & VanillaPress #############################################################################
+// Week 04 - Dev Tools, Workflows & VanillaPress ####################################
+
+// 1. Write a two helper functions getLocalStore() and updateLocalStore() that can take an array or object, stringify it and save to local storage as well as pull content out of local storage and parse it.
+
+// 2. Write a helper function that renders posts to the page using the following markup:
+// <article class="post">
+//  <h2 class="entry-title"><a href="#slug">Title</a></h2>
+//  <div class="entry-content">
+//    <p>Content here...</p>
+//  </div>  
+// </article>
+
+// 3. Write an init() function that gets posts from JSON and saves them to local store. Then get's content from local store and renders to the page. Use the functions created in #1 and #2.
+
+// 4. Grab the collection of pages from JSON and create a function that returns an unordered list of the pages.
+
+// 5. Modify the list of pages to include a slug like the following that logs out the link when it is clicked
+// <li><a href="#1">Post #1</a></li>
+// <li><a href="#2">Post #2</a></li>
+
+// 6. When you click on the link in the navigation list from #5, find the corresponding post from the list of posts from the JSON list and log it to the screen.
+
+// 7. Modify the navigation menu so that when you click on a link the entire post is displayed.
+
+// Optional #########################################################################
