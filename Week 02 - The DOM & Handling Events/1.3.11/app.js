b5be2f424854323da95d18bc104d88df06104448
Niels Lange <info@nielslange.de>
2019-02-04 17:23:24 +0700

    Added examples 1.3.11 to week 02

---
+++ Week 02 - The DOM & Handling Events/1.3.11/app.js
@@ -0,0 +1,101 @@
+/********************************
+ * Cloning Nodes
+ * 1.3.11
+ *
+ *******************************/
+
+
+
+
+/********************************
+ * Cloning a Node
+ * 1.3.11.1
+ *
+ *******************************/
+
+// var fieldsList = document.querySelector( '.fields' ),
+//     firstField = fieldsList.children[0],
+//     secondField = firstField.cloneNode(),
+//     thirdField = firstField.cloneNode( true );
+//
+// fieldsList.appendChild( secondField );
+// fieldsList.appendChild( thirdField );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Cloning a Node and Updating
+ * Values
+ * 1.3.11.2
+ *
+ *******************************/
+
+// var fieldsList = document.querySelector( '.fields' ),
+//     firstField = fieldsList.children[0],
+//     firstFieldInput = firstField.querySelector( 'input' ),
+//     firstFieldIdStr = firstFieldInput.getAttribute( 'id' ),
+//     secondField = firstField.cloneNode( true ),
+//     secondFieldInput = secondField.querySelector( 'input' ),
+//     secondFieldLabel = secondField.querySelector( 'label' ),
+//     secondFieldIdStr = updateIdStr( firstFieldIdStr );
+//
+// secondFieldInput.setAttribute( 'id', secondFieldIdStr );
+// secondFieldInput.setAttribute( 'name', secondFieldIdStr );
+// secondFieldLabel.setAttribute( 'for', secondFieldIdStr );
+//
+// fieldsList.appendChild( secondField );
+//
+// function updateIdStr( value ) {
+//
+//   var strArray = value.split( '-' ),
+//       id = strArray[ strArray.length - 1 ],
+//       newId = parseInt( id ) + 1;
+//
+//   strArray[ strArray.length - 1 ] = newId;
+//   return strArray.toString().replace( ',' , '-' );
+//
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// End 1.3.11
