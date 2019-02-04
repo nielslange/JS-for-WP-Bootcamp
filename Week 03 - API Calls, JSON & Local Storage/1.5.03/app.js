27717cc0a8d8d870e35e6f7cbabb954fb59c4f4c
Niels Lange <info@nielslange.de>
2019-02-04 19:29:47 +0700

    Added examples 1.5.3 to week 03

---
+++ Week 03 - API Calls, JSON & Local Storage/1.5.03/app.js
@@ -0,0 +1,214 @@
+/********************************
+ * Local Storage
+ * 1.5.3
+ *
+ *******************************/
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Set Local Storage
+ * 1.5.3.1
+ *
+ *******************************/
+
+ // var siteName = 'My Site';
+
+// localStorage.setItem( 'siteName', siteName );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Get Local Storage
+ * 1.5.3.2
+ *
+ *******************************/
+
+ // var header = document.getElementsByTagName( 'h1' )[0],
+//     siteName;
+
+// localStorage.setItem( 'siteName', 'My Site' );
+
+// siteName = localStorage.getItem( 'siteName' );
+// header.innerHTML = siteName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Remove Local Storage
+ * 1.5.3.3
+ *
+ *******************************/
+
+ // var header = document.getElementsByTagName( 'h1' )[0],
+//     siteName;
+
+// localStorage.setItem( 'siteName', 'My Updated Site' );
+
+// siteName = localStorage.getItem( 'siteName' );
+// header.innerHTML = siteName;
+
+// localStorage.removeItem( 'siteName' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Adding Multiple Items to
+ * Local Storage
+ * 1.5.3.4
+ *
+ *******************************/
+
+// var siteName = 'My Site',
+//     siteDescription = 'Another JS Site';
+
+// localStorage.setItem( 'siteName', siteName );
+// localStorage.setItem( 'siteDescription', siteDescription );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/********************************
+ * Adding Multiple Items to
+ * Local Storage with JSON
+ * 1.5.3.5
+ *
+ *******************************/
+
+// var header = document.getElementsByTagName( 'h1' )[0],
+//     siteData = {
+//       siteName: 'My Site',
+//       siteDescription: 'Another JS Site'
+//     },
+//     localData;
+
+
+// localStorage.setItem( 'siteData', JSON.stringify( siteData ) );
+
+// localData = JSON.parse( localStorage.getItem( 'siteData' ) );
+
+// console.log( localData );
+// console.log( localStorage.getItem( 'siteData' ) );
+
+
+// header.innerHTML = localData.siteName;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// End 1.5.3
